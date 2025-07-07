404e79652bfe7162c910ac05c079124a2a4efaaa
anjazunjic <zunjicanja35@gmail.com>
2025-07-07 22:12:09 +0200

    commit

--- healthcheck.py
+++ healthcheck.py
@@ -4,21 +4,19 @@ import mysql.connector
 app = Flask(__name__)
 
 @app.route('/')
-def health_check():
-    try:
-        # Poveži se sa MySQL serverom
-        conn = mysql.connector.connect(
-            host="localhost",  # MySQL server je u istom kontejneru
-            user="root",
-            password="MySQLPassword",
-            database="sistem_za_regrutaciju"
-        )
-        if conn.is_connected():
-            return "MySQL server is up and running!"
-        else:
-            return "Failed to connect to MySQL."
-    except Exception as e:
-        return f"Error: {str(e)}"
+def healthcheck():
+    # Poveži se na MySQL
+    connection = mysql.connector.connect(
+        host="mysql",  # Ime servisa iz docker-compose.yml
+        user="root",
+        password="MySQLPassword",
+        database="sistem_za_regrutaciju"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT 'Hello from Flask and MySQL!'")
+    result = cursor.fetchone()
+    connection.close()
+    return result[0]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
